97c1273d9954f68bf42b3b63c6d8ccf348d1e8ef
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2025-01-17 09:18:25 +0530

    Schema for books and user created

---
+++ server/models/Books.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const BookSchema = new Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  tag: { type: String, default: "General" },
+  
+  date: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('books', BookSchema);
